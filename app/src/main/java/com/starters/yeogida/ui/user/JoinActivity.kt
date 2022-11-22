29d5ed49901a91465432b97c75b8fe0a01c3a69b
YOON <yys7517@naver.com>
2022-11-22 14:59:21 +0900

    [STYLE] 회원가입 화면 디자인 레이아웃 완료 ui/#9

--- app/src/main/java/com/starters/yeogida/ui/user/JoinActivity.kt
+++ app/src/main/java/com/starters/yeogida/ui/user/JoinActivity.kt
@@ -1,12 +1,14 @@
 package com.starters.yeogida.ui.user
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.starters.yeogida.R
 
 class JoinActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
+
+        // TODO. 중복닉네임이면 닉네임 입력창 테두리 빨갛게
     }
 }

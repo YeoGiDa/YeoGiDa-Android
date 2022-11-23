d214f69a84809e9c00d8ce8779c331f7db98dd17
hjh1161514 <hjh1161514@naver.com>
2022-11-23 16:25:35 +0900

    [FEAT] Bottom navigation 추가 ui/#17

--- app/src/main/java/com/starters/yeogida/MainActivity.kt
+++ app/src/main/java/com/starters/yeogida/MainActivity.kt
@@ -2,10 +2,26 @@ package com.starters.yeogida
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.starters.yeogida.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        initBottomNavigation()
+
+        setContentView(binding.root)
+    }
+
+    private fun initBottomNavigation() {
+        val navController = supportFragmentManager.findFragmentById(R.id.container_main)?.findNavController()
+        navController?.let { // null이 아닐 때만 확인하기 위해 let 사용
+            binding.navigationMain.setupWithNavController(it)
+        }
     }
 }

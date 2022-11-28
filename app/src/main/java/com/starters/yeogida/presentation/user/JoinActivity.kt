5197e71e6e6088029bcb7157f927419c8b40d250
YOON <yys7517@naver.com>
2022-11-28 10:43:48 +0900

    [FEAT] 로그인 성공 시 회원가입 화면 이동 및 데이터 전달 - 로그인 성공 시 가입되어 있는 사용자인지 확인 후 액티비티 전환 - 가입되어있지 않은 사용자일때, 동의한 항목에 따라 회원가입 화면에 데이터 전달 - 닉네임 입력 EditText 이벤트 설정 feature/#8

--- app/src/main/java/com/starters/yeogida/presentation/user/JoinActivity.kt
+++ app/src/main/java/com/starters/yeogida/presentation/user/JoinActivity.kt
@@ -1,14 +1,123 @@
 package com.starters.yeogida.presentation.user
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.InputFilter
+import android.text.TextWatcher
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.databinding.DataBindingUtil
+import com.kakao.sdk.user.UserApiClient
+import com.starters.yeogida.GlideApp
 import com.starters.yeogida.R
+import com.starters.yeogida.databinding.ActivityJoinBinding
+import java.util.regex.Pattern
 
 class JoinActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityJoinBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_join)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_join)
 
-        // TODO. 중복닉네임이면 닉네임 입력창 테두리 빨갛게
+        setNicknameFilter()
+        setTextChangedListener()
+        setNickname()
+        setProfileImage()
+
+        binding.btnJoinSubmit.setOnClickListener {
+            if ( hasSameNickname() ) {
+                binding.tvProfileDescription.visibility = View.VISIBLE  // 중복된다는 문구 보이기.
+
+                with(binding.etNick) {
+                    text.clear()
+                    setBackgroundResource(R.drawable.rectangle_border_red_10)
+                    requestFocus()
+                    keyboardShow()
+                }
+                Toast.makeText(this, "닉네임이 중복됩니다.", Toast.LENGTH_SHORT).show()
+            } else {
+                // TODO. 회원등록 API
+                intent.getStringExtra("email")?.let {
+                    Log.d("JoinActivity", "이메일 : $it")
+                }
+            }
+        }
+    }
+
+    private fun hasSameNickname() : Boolean{
+        var hasSameNick = false
+        // TODO. 서버 중복확인
+        return hasSameNick
+    }
+
+    private fun setTextChangedListener() {
+        binding.etNick.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                // 완료 버튼 Enabled 이벤트
+                binding.btnJoinSubmit.isEnabled = !binding.etNick.text.isNullOrBlank()
+
+                if(binding.etNick.text.length > 8)
+                    Toast.makeText(this@JoinActivity, "닉네임은 8글자를 넘을 수 없습니다.", Toast.LENGTH_SHORT)
+                        .show()
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun afterTextChanged(p0: Editable?) {}
+        })
+    }
+
+    private fun setNickname() {
+        intent.getStringExtra("nickname")?.let {
+            Log.d("JoinActivity", "닉네임 : $it")
+            with(binding.etNick) {
+                requestFocus()  // Focusing 하기
+                setText(it)
+                setSelection(length())  // 커서 끝으로
+
+                // 키보드도 올라오게
+                keyboardShow()
+            }
+        }
+    }
+
+    private fun setProfileImage() {
+        intent.getStringExtra("profileImageUrl")?.let { // 프로필 이미지 URL
+            Log.d("JoinActivity", "사진 url : $it")
+            GlideApp.with(this)
+                .load(it)
+                .circleCrop()
+                .into(binding.ivProfile)
+        }
+    }
+
+    private fun setNicknameFilter() {
+        /** 문자열필터(EditText Filter) */
+        var filterAlphaNumSpace = InputFilter { source, start, end, dest, dstart, dend ->
+            /*
+                [요약 설명]
+                1. 정규식 패턴 ^[a-z] : 영어 소문자 허용
+                2. 정규식 패턴 ^[A-Z] : 영어 대문자 허용
+                3. 정규식 패턴 ^[ㄱ-ㅣ가-힣] : 한글 허용
+                4. 정규식 패턴 ^[0-9] : 숫자 허용
+                5. 정규식 패턴 ^[ ] or ^[\\s] : 공백 허용
+            */
+            val ps = Pattern.compile("^[ㄱ-ㅣ가-힣a-zA-Z0-9\\s]+$")
+            if (!ps.matcher(source).matches()) {
+                ""
+            } else source
+        }
+        var lengthFilter = InputFilter.LengthFilter(8)
+
+        binding.etNick.filters = arrayOf(filterAlphaNumSpace, lengthFilter)
+    }
+
+    private fun keyboardShow() {
+        WindowInsetsControllerCompat(window, window.decorView).show(WindowInsetsCompat.Type.ime())
     }
 }
--- app/src/main/java/com/starters/yeogida/presentation/user/LoginActivity.kt
+++ app/src/main/java/com/starters/yeogida/presentation/user/LoginActivity.kt
@@ -1,58 +1,96 @@
 package com.starters.yeogida.presentation.user
 
 import android.content.Intent
-import android.content.Intent.FLAG_ACTIVITY_CLEAR_TOP
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.kakao.sdk.auth.model.OAuthToken
-import com.kakao.sdk.common.KakaoSdk
 import com.kakao.sdk.user.UserApiClient
+import com.starters.yeogida.MainActivity
 import com.starters.yeogida.R
 import com.starters.yeogida.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityLoginBinding
+    private lateinit var binding: ActivityLoginBinding
+
+    private val mCallback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+        if (error != null) {
+            Log.e("LoginActivity", "로그인 실패 $error")
+        } else if (token != null) {
+            Log.e("LoginActivity", "로그인 성공 ${token.accessToken}")
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
 
         binding.layoutLoginKakao.setOnClickListener {
+            if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
+                loginWithKakaoTalk()
+            } else {
+                UserApiClient.instance.loginWithKakaoAccount(this, callback = mCallback)
+            }
+        }
+    }
 
-            UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
-                if (error != null) {
-                    Log.e("LoginActivity", "로그인 실패", error)
-                }
-                else if (token != null) {
-                    Log.i("LoginActivity", "로그인 성공 ${token.accessToken}")
+    private fun loginWithKakaoTalk() {
+        UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
+            if (error != null) {
+                Log.e("LoginActivity", "로그인 실패", error)
+            } else if (token != null) {
+                Log.i("LoginActivity", "로그인 성공 ${token.accessToken}")
 
-                    UserApiClient.instance.accessTokenInfo { tokenInfo, error ->
-                        if (error != null) {
-                            Log.e("LoginActivity", "토큰 정보 보기 실패", error)
-                        }
-                        else if (tokenInfo != null) {
-                            Log.i("LoginActivity", "토큰 정보 보기 성공" +
+                UserApiClient.instance.accessTokenInfo { tokenInfo, error ->
+                    if (error != null) {
+                        Log.e("LoginActivity", "토큰 정보 보기 실패", error)
+                    } else if (tokenInfo != null) {
+                        Log.i(
+                            "LoginActivity", "토큰 정보 보기 성공" +
                                     "\n회원번호: ${tokenInfo.id}" +
                                     "\n만료시간: ${tokenInfo.expiresIn} 초" +
-                                    "\n앱 ID : ${tokenInfo.appId}")
+                                    "\n앱 ID : ${tokenInfo.appId}"
+                        )
+                    }
+                }
+
+                UserApiClient.instance.me { user, error ->
+                    user?.kakaoAccount?.let { userAccount ->
+
+                        val email = userAccount.email.toString().trim()
+
+                        if( isJoinedMember(email) ) {
+                            val intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        } else  { // 가입 안되어있으면
+                            val intent = Intent(this, JoinActivity::class.java)
+                            intent.putExtra("email", email)
+
+                            // 닉네임
+                            userAccount.profile?.nickname?.let {
+                                intent.putExtra( "nickname", it.trim())
+                            }
+
+                            // 프로필 사진
+                            userAccount.profile?.profileImageUrl?.let {
+                                intent.putExtra("profileImageUrl", it.trim())
+                            }
+
+                            startActivity(intent)
+                            finish()
                         }
                     }
-                    /*
-                    UserApiClient.instance.unlink { error ->
-                        if (error != null) {
-                            Toast.makeText(this, "회원 탈퇴 실패 $error", Toast.LENGTH_SHORT).show()
-                        }else {
-                            Toast.makeText(this, "회원 탈퇴 성공", Toast.LENGTH_SHORT).show()
-                        }
-                    }*/
                 }
+
             }
-
-
         }
+    }
 
+    private fun isJoinedMember( email : String ) : Boolean {
+        var isJoined = false
+        // TODO. 서버 이메일 전달해서 가입되어있는 사용자인지 확인하기
+        return isJoined
     }
 }

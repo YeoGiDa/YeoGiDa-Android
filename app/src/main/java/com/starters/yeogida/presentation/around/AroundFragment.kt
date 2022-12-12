89b6420d5cf11c7ee88d2d072cf14d4b0af9099a
hjh1161514 <hjh1161514@naver.com>
2022-12-12 14:06:22 +0900

    [FEAT] 위치 권한, 현재 위치 - 권한에 따른 로직 - 현재 위치 - 첫 화면 (0,0) 변경 feature/#48

--- app/src/main/java/com/starters/yeogida/presentation/around/AroundFragment.kt
+++ app/src/main/java/com/starters/yeogida/presentation/around/AroundFragment.kt
@@ -1,28 +1,40 @@
 package com.starters.yeogida.presentation.around
 
 import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.net.Uri
 import android.os.Bundle
+import android.os.Looper
+import android.provider.Settings
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Marker
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.starters.yeogida.BuildConfig
 import com.starters.yeogida.data.local.PlaceBottomSheetData
 import com.starters.yeogida.databinding.FragmentAroundBinding
 
 class AroundFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
-    private lateinit var mView: MapView
     private lateinit var binding: FragmentAroundBinding
+    private lateinit var mView: MapView
     private lateinit var mMap: GoogleMap
     private lateinit var placeBottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationCallback: LocationCallback
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,27 +45,78 @@ class AroundFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickLi
 
         mView = binding.mapViewAround
         mView.onCreate(savedInstanceState)
-        mView.getMapAsync(this)
+        mView.getMapAsync {
+            val center = LatLng(36.38, 127.51)
+            it.moveCamera(CameraUpdateFactory.newLatLng(center))
+            it.moveCamera(CameraUpdateFactory.zoomTo(7f))
+        }
+
         setPlaceBottomSheet()
 
         return binding.root
     }
 
+    private fun startProcess() {
+        mView.getMapAsync(this)
+    }
+
     @SuppressLint("PotentialBehaviorOverride")
     override fun onMapReady(googleMap: GoogleMap) {
+        Log.d("###", "onMapReady")
         mMap = googleMap
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
+        updateLocation()
         mMap.setOnMarkerClickListener(this)
         mMap.setOnMapClickListener {
             placeBottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
         }
-        createMarker()
     }
 
-    private fun createMarker() {
-        val marker = LatLng(37.570286992195, 126.98361037914)
-        mMap.addMarker(MarkerOptions().position(marker).title("스타터스"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(marker))
-        mMap.moveCamera(CameraUpdateFactory.zoomTo(15f))
+    @SuppressLint("MissingPermission")
+    private fun updateLocation() {
+
+        val locationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 600000) // 위치 정보를 요청할 정확도와 주기 설정
+            .setWaitForAccurateLocation(false)
+            .build()
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(p0: LocationResult) {
+                locationRequest.let {
+                    for ((i, location) in p0.locations.withIndex()) {
+                        setLastLocation(location)
+                    }
+                }
+            }
+        }
+
+        fusedLocationClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            Looper.myLooper()
+        )
+    }
+
+    @SuppressLint("MissingPermission")
+    // 위치 정보를 받아서 마커를 그리고 화면 이동
+    fun setLastLocation(lastLocation: Location) {
+        val latLng = LatLng(lastLocation.latitude, lastLocation.longitude)
+        val cameraPosition = CameraPosition.Builder()
+            .target(latLng)
+            .zoom(15f)
+            .build()
+        Log.d("###", latLng.toString())
+        mMap.clear()
+        // mMap.addMarker(markerOptions)
+        mMap.isMyLocationEnabled = true
+        mMap.uiSettings.isMyLocationButtonEnabled = true
+        mMap.setPadding(20, 200, 20, 20)
+        mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+    }
+
+    override fun onDestroyView() {
+        // TODO: 허용을 해야 생성
+        // fusedLocationClient.removeLocationUpdates(locationCallback)
+        super.onDestroyView()
     }
 
     override fun onMarkerClick(p0: Marker): Boolean {
@@ -115,8 +178,78 @@ class AroundFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickLi
         }
     }
 
+    // 권한 처리
+    private val permissions = arrayOf(
+        android.Manifest.permission.ACCESS_COARSE_LOCATION,
+        android.Manifest.permission.ACCESS_FINE_LOCATION
+    )
+    private val permLocation = 99
+
+    // 권한 요청 전에 검사하는 코드
+    // 두 권한 중 하나라도 승인되어 있지 않으면 다시 요청
+    private fun checkPermission() {
+        var permittedAll = true
+        for (permission in permissions) {
+            val result = ContextCompat.checkSelfPermission(requireContext(), permission)
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                permittedAll = false
+                requestPermission()
+                break
+            }
+        }
+        if (permittedAll) {
+            startProcess()
+        }
+    }
+
+    private fun requestPermission() {
+        Log.d("###", "requestPermission")
+        requestPermissions(permissions, permLocation)
+    }
+
+    // 권한이 승인되지 않으면 한 번 더 확인
+    private fun confirmAgain() {
+        Log.d("###", "confirmAgain")
+        AlertDialog.Builder(requireContext())
+            .setTitle("권한 승인 확인")
+            .setMessage("위치 관련 권한을 모두 승인하셔야 현재 위치 주변 장소를 확인할 수 있습니다. 권한 승인을 다시 하시겠습니까?")
+            .setPositiveButton("네") { _, _ ->
+                val intent =
+                    Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                        .setData(Uri.parse("package:" + BuildConfig.APPLICATION_ID))
+                startActivity(intent)
+            }.setNegativeButton("아니요") { _, _ ->
+            }.create()
+            .show()
+    }
+
+    // 모두 다 승인이면 프로세스 시작, 아니면 confirmAgain 호출
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            99 -> {
+                var grantedAll = true
+                for (result in grantResults) {
+                    if (result != PackageManager.PERMISSION_GRANTED) {
+                        grantedAll = false
+                        break
+                    }
+                }
+                if (grantedAll) {
+                    startProcess()
+                } else {
+                    confirmAgain()
+                }
+            }
+        }
+    }
+
     override fun onStart() {
         super.onStart()
+        checkPermission()
         mView.onStart()
     }
 

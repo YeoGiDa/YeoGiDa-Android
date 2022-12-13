ab7eb32c170cd1dfc130fd62ee00bbb9d1270d45
hjh1161514 <hjh1161514@naver.com>
2022-12-13 12:12:14 +0900

    [FEAT] 갤러리 연동 feature/#63

--- app/src/main/java/com/starters/yeogida/presentation/trip/AddTripFragment.kt
+++ app/src/main/java/com/starters/yeogida/presentation/trip/AddTripFragment.kt
@@ -1,7 +1,9 @@
 package com.starters.yeogida.presentation.trip
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.util.Log
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,13 +11,14 @@ import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import com.starters.yeogida.R
 import com.starters.yeogida.databinding.FragmentAddTripBinding
 import com.starters.yeogida.util.shortToast
 
-
 class AddTripFragment : Fragment() {
     private lateinit var binding: FragmentAddTripBinding
+    private val pickImage = 1
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,7 +30,6 @@ class AddTripFragment : Fragment() {
 
         initBottomSheet()
         checkMaxLength()
-        initNavigation()
 
         return binding.root
     }
@@ -57,10 +59,29 @@ class AddTripFragment : Fragment() {
         }
     }
 
-    private fun initNavigation() {
-        binding.btnAddTripNext.setOnClickListener {
-            findNavController().navigate(R.id.action_add_trip_to_around_place)
+    var selectedPicUri: Uri? = null
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == pickImage) {
+            data?.let {
+                selectedPicUri = it.data!!
+                binding.ivAddTripPhoto.visibility = View.VISIBLE
+                binding.ivAddTripPhotoX.visibility = View.VISIBLE
+                binding.tvAddTripPhotoDescription.visibility = View.VISIBLE
+                Glide.with(this).load(selectedPicUri).into(binding.ivAddTripPhoto)
+            }
         }
+    }
+
+    fun moveToAroundPlace(view: View) {
+        findNavController().navigate(R.id.action_add_trip_to_around_place)
+    }
+
+    fun connectGallery(view: View) {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = MediaStore.Images.Media.CONTENT_TYPE
+        startActivityForResult(intent, pickImage)
     }
 
     fun close(view: View) {

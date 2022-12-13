52d31c39d27c374b0eca24c6e2270e88b569609c
hjh1161514 <hjh1161514@naver.com>
2022-12-13 14:40:59 +0900

    [FEAT] 사진 삭제, 완료 버튼 활성화 feature/#63

--- app/src/main/java/com/starters/yeogida/presentation/trip/AddTripFragment.kt
+++ app/src/main/java/com/starters/yeogida/presentation/trip/AddTripFragment.kt
@@ -4,9 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -30,6 +28,7 @@ class AddTripFragment : Fragment() {
 
         initBottomSheet()
         checkMaxLength()
+        activeConfirmButton()
 
         return binding.root
     }
@@ -39,6 +38,7 @@ class AddTripFragment : Fragment() {
             // binding.tvAddTripRegion.setBackgroundResource(R.drawable.rectangle_border_main_blue_10)
             val bottomSheetDialog = RegionBottomSheetFragment {
                 binding.tvAddTripRegion.text = it
+                activeConfirmButton()
             }
             bottomSheetDialog.show(parentFragmentManager, bottomSheetDialog.tag)
         }
@@ -47,15 +47,24 @@ class AddTripFragment : Fragment() {
     private fun checkMaxLength() {
         with(binding) {
             etAddTripName.addTextChangedListener {
+                activeConfirmButton()
                 if (etAddTripName.text.length == 10) {
                     requireContext().shortToast("최대 10글자까지 작성 가능합니다.")
                 }
             }
             etAddTripSubtitle.addTextChangedListener {
+                activeConfirmButton()
                 if (etAddTripSubtitle.text.length == 10) {
                     requireContext().shortToast("최대 10글자까지 작성 가능합니다.")
                 }
             }
+        }
+    }
+
+    private fun activeConfirmButton() {
+        with(binding) {
+            btnAddTripNext.isEnabled =
+                !tvAddTripRegion.text.isNullOrEmpty() && !etAddTripName.text.isNullOrEmpty() && !etAddTripSubtitle.text.isNullOrEmpty() && ivAddTripPhotoX.visibility == View.VISIBLE
         }
     }
 
@@ -70,6 +79,7 @@ class AddTripFragment : Fragment() {
                 binding.ivAddTripPhotoX.visibility = View.VISIBLE
                 binding.tvAddTripPhotoDescription.visibility = View.VISIBLE
                 Glide.with(this).load(selectedPicUri).into(binding.ivAddTripPhoto)
+                activeConfirmButton()
             }
         }
     }
@@ -82,6 +92,13 @@ class AddTripFragment : Fragment() {
         val intent = Intent(Intent.ACTION_PICK)
         intent.type = MediaStore.Images.Media.CONTENT_TYPE
         startActivityForResult(intent, pickImage)
+    }
+
+    fun deletePhoto(view: View) {
+        binding.ivAddTripPhoto.visibility = View.GONE
+        binding.ivAddTripPhotoX.visibility = View.GONE
+        binding.tvAddTripPhotoDescription.visibility = View.GONE
+        activeConfirmButton()
     }
 
     fun close(view: View) {

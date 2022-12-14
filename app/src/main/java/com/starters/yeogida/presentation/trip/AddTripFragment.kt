2a5419e163c1f3e87482716ed8be697374cce316
hjh1161514 <hjh1161514@naver.com>
2022-12-14 09:23:19 +0900

    [FEAT] 장소 지도로 보기 activity 연결 feature/#64

--- app/src/main/java/com/starters/yeogida/presentation/trip/AddTripFragment.kt
+++ app/src/main/java/com/starters/yeogida/presentation/trip/AddTripFragment.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,11 +13,11 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.starters.yeogida.R
 import com.starters.yeogida.databinding.FragmentAddTripBinding
 import com.starters.yeogida.presentation.common.ImageActivity
+import com.starters.yeogida.presentation.place.PlaceActivity
 import com.starters.yeogida.util.shortToast
 
 class AddTripFragment : Fragment() {
@@ -107,7 +106,8 @@ class AddTripFragment : Fragment() {
         }
 
     fun moveToAroundPlace(view: View) {
-        findNavController().navigate(R.id.action_add_trip_to_around_place)
+        val intent = Intent(activity, PlaceActivity::class.java)
+        startActivity(intent)
     }
 
     // 갤러리 창 연결

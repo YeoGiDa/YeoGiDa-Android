9bfa27c91a7844be4817c9d73b7aac049cf63095
hjh1161514 <hjh1161514@naver.com>
2022-12-14 10:34:23 +0900

    [FEAT] 맨 위로 가기 feature/#64

--- app/src/main/java/com/starters/yeogida/presentation/around/AroundPlaceFragment.kt
+++ app/src/main/java/com/starters/yeogida/presentation/around/AroundPlaceFragment.kt
@@ -8,11 +8,13 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearSmoothScroller
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.SmoothScroller
 import com.starters.yeogida.R
 import com.starters.yeogida.data.local.PlaceData
 import com.starters.yeogida.databinding.FragmentAroundPlaceBinding
 import com.starters.yeogida.presentation.place.PlaceActivity
-import com.starters.yeogida.presentation.trip.AddTripActivity
 
 class AroundPlaceFragment : Fragment() {
     private lateinit var binding: FragmentAroundPlaceBinding
@@ -47,6 +49,24 @@ class AroundPlaceFragment : Fragment() {
                 PlaceData("https://cdn.pixabay.com/photo/2017/08/07/23/11/iceland-2608985_1280.jpg", "오오폭포", "3.0", 0, "바"),
                 PlaceData("https://cdn.pixabay.com/photo/2016/04/26/03/55/salmon-1353598_1280.jpg", "연어맛집", "4.5", 30, "쇼핑"),
                 PlaceData("https://cdn.pixabay.com/photo/2013/04/11/19/46/building-102840_1280.jpg", "박물관", "2.0", 1, "숙소"),
+                PlaceData("https://cdn.pixabay.com/photo/2017/08/07/23/11/iceland-2608985_1280.jpg", "오오폭포", "3.0", 0, "기타"),
+                PlaceData("https://cdn.pixabay.com/photo/2016/04/26/03/55/salmon-1353598_1280.jpg", "연어맛집", "4.5", 30, "식당"),
+                PlaceData("https://cdn.pixabay.com/photo/2013/04/11/19/46/building-102840_1280.jpg", "박물관", "2.0", 1, "카페"),
+                PlaceData("https://cdn.pixabay.com/photo/2017/08/07/23/11/iceland-2608985_1280.jpg", "오오폭포", "3.0", 0, "바"),
+                PlaceData("https://cdn.pixabay.com/photo/2016/11/21/13/04/alcoholic-beverages-1845295_1280.jpg", "수울수울", "1.5", 999, "관광지"),
+                PlaceData("https://cdn.pixabay.com/photo/2013/04/11/19/46/building-102840_1280.jpg", "박물관", "2.0", 1, "카페"),
+                PlaceData("https://cdn.pixabay.com/photo/2017/08/07/23/11/iceland-2608985_1280.jpg", "오오폭포", "3.0", 0, "바"),
+                PlaceData("https://cdn.pixabay.com/photo/2016/04/26/03/55/salmon-1353598_1280.jpg", "연어맛집", "4.5", 30, "쇼핑"),
+                PlaceData("https://cdn.pixabay.com/photo/2013/04/11/19/46/building-102840_1280.jpg", "박물관", "2.0", 1, "숙소"),
+                PlaceData("https://cdn.pixabay.com/photo/2017/08/07/23/11/iceland-2608985_1280.jpg", "오오폭포", "3.0", 0, "기타"),
+                PlaceData("https://cdn.pixabay.com/photo/2016/04/26/03/55/salmon-1353598_1280.jpg", "연어맛집", "4.5", 30, "식당"),
+                PlaceData("https://cdn.pixabay.com/photo/2013/04/11/19/46/building-102840_1280.jpg", "박물관", "2.0", 1, "카페"),
+                PlaceData("https://cdn.pixabay.com/photo/2017/08/07/23/11/iceland-2608985_1280.jpg", "오오폭포", "3.0", 0, "바"),
+                PlaceData("https://cdn.pixabay.com/photo/2016/11/21/13/04/alcoholic-beverages-1845295_1280.jpg", "수울수울", "1.5", 999, "관광지"),
+                PlaceData("https://cdn.pixabay.com/photo/2013/04/11/19/46/building-102840_1280.jpg", "박물관", "2.0", 1, "카페"),
+                PlaceData("https://cdn.pixabay.com/photo/2017/08/07/23/11/iceland-2608985_1280.jpg", "오오폭포", "3.0", 0, "바"),
+                PlaceData("https://cdn.pixabay.com/photo/2016/04/26/03/55/salmon-1353598_1280.jpg", "연어맛집", "4.5", 30, "쇼핑"),
+                PlaceData("https://cdn.pixabay.com/photo/2013/04/11/19/46/building-102840_1280.jpg", "박물관", "2.0", 1, "숙소"),
                 PlaceData("https://cdn.pixabay.com/photo/2017/08/07/23/11/iceland-2608985_1280.jpg", "오오폭포", "3.0", 0, "기타")
             )
         )
@@ -61,6 +81,19 @@ class AroundPlaceFragment : Fragment() {
 
     fun moveToPlaceMap(view: View) {
         findNavController().navigate(R.id.action_aroundPlace_to_placeMap)
+    }
+
+    fun RecyclerView.smoothSnapToPosition(position: Int, snapMode: Int = LinearSmoothScroller.SNAP_TO_START) {
+        val smoothScroller = object : LinearSmoothScroller(this.context) {
+            override fun getVerticalSnapPreference(): Int = snapMode
+            override fun getHorizontalSnapPreference(): Int = snapMode
+        }
+        smoothScroller.targetPosition = position
+        layoutManager?.startSmoothScroll(smoothScroller)
+    }
+
+    fun moveToTop(view: View) {
+        binding.scrollViewAroundPlace.smoothScrollTo(0, 0)
     }
 
     fun close(view: View) {

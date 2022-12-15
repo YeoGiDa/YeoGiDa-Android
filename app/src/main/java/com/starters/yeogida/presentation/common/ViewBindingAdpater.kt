cdd4be24bcd21e32d1a5598fe8cf38f82f211f52
hjh1161514 <hjh1161514@naver.com>
2022-12-16 00:36:44 +0900

    [FEAT] Visibility gone일 때 추가 network/#65

--- app/src/main/java/com/starters/yeogida/presentation/common/ViewBindingAdpater.kt
+++ app/src/main/java/com/starters/yeogida/presentation/common/ViewBindingAdpater.kt
@@ -1,6 +1,7 @@
 package com.starters.yeogida.presentation.common
 
 import android.view.View
+import android.widget.TextView
 import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
 
@@ -9,7 +10,12 @@ fun updateVisibility(view: View, isLike: Boolean) {
     view.isSelected = isLike
 }
 
+@BindingAdapter("writerId", "myId")
+fun userViewVisibility(view: TextView, writerId: Long, myId: Long) {
+    view.isVisible = writerId == myId
+}
+
 @BindingAdapter("writerId", "memberId")
-fun userViewVisibility(view: View, writerId: Long, memberId: Long) {
-    view.isVisible = writerId == memberId
+fun userViewGone(view: TextView, writerId: Long, memberId: Long) {
+    view.isVisible = writerId != memberId
 }

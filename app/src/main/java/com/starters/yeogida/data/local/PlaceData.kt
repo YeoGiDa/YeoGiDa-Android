97bc26a468b1146d4720031e2d28efb3ce69c852
hjh1161514 <hjh1161514@naver.com>
2022-12-16 10:31:12 +0900

    [FEAT] 장소 목록 조회 api response 데이터 추가 network/#50

--- app/src/main/java/com/starters/yeogida/data/local/PlaceData.kt
+++
@@ -1,9 +0,0 @@
-package com.starters.yeogida.data.local
-
-data class PlaceData(
-    val placeThumbnail: String,
-    val placeName: String,
-    val star: String,
-    val comment: Int,
-    val tag: String
-)

d50ca776d3ceed378db8eefcd40a86fd4772c391
YOON <yys7517@naver.com>
2022-12-19 21:42:56 +0900

    [FEAT] 장소 추가 시 장소 이름 외 나머지 입력 값 처리 - Chip Button 태그 값 받아오기 및 예외 처리 - 별점 값 받아오기 및 예외 처리 - 리뷰 내용 받아오기 및 예외 처리 feat/#85

--- app/src/main/java/com/starters/yeogida/presentation/place/AddPlaceActivity.kt
+++ app/src/main/java/com/starters/yeogida/presentation/place/AddPlaceActivity.kt
@@ -7,7 +7,10 @@ import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
+import android.widget.RatingBar
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.viewModels
@@ -16,6 +19,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
+import com.google.android.material.chip.Chip
 import com.starters.yeogida.BuildConfig
 import com.starters.yeogida.R
 import com.starters.yeogida.YeogidaApplication
@@ -29,6 +33,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.io.File
 
 class AddPlaceActivity : AppCompatActivity(), PlaceImageClickListener {
     private lateinit var binding: ActivityAddPlaceBinding
@@ -41,6 +46,18 @@ class AddPlaceActivity : AppCompatActivity(), PlaceImageClickListener {
 
     private val placeImageUriList = mutableListOf<Uri>()
 
+    private var isTagSelected: Boolean = false
+
+    private var placeId: String? = null
+    private var placeTitle: String? = null
+    private var placeAddress: String? = null
+    private var placeStar: Float? = null
+    private var placeReviewContent: String? = null
+    private var placeLongitude: Double? = null
+    private var placeLatitude: Double? = null
+    private var placeTag: String? = null
+    private var placeImageList = mutableListOf<File?>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_add_place)
@@ -50,7 +67,85 @@ class AddPlaceActivity : AppCompatActivity(), PlaceImageClickListener {
         setOnBackPressed() // 뒤로가기 리스너
 
         setPlaceImageAdapter()
-        onAddPhotoButtonClicked() // 장소 추가 버튼 클릭
+        onAddPhotoButtonClicked() // 장소 사진 추가 버튼 클릭
+        setTextChangedListener()    // 리뷰 TextChangedListener
+        setPlaceRating()   // 별점 ChangedListener
+        setPlaceTag()       // 장소 태그
+
+        setOnSubmitButtonClicked()  // 완료 버튼 클릭 시, 주소 값, 태그 값
+    }
+
+    private fun setTextChangedListener() {
+        binding.etAddPlaceReview.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                // 완료 버튼 Enabled 이벤트
+                with(binding.etAddPlaceReview) {
+                    val length = this.length()
+                    if (length == 200) {
+                        shortToast("리뷰는 200자를 넘을 수 없습니다.")
+                    } else {
+                        binding.tvAddPlaceReviewTextCount.text = "$length / 200"
+                        activeSubmitButton()
+                    }
+                }
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun afterTextChanged(p0: Editable?) {}
+        })
+    }
+
+    private fun activeSubmitButton() {
+        with(binding) {
+            btnAddPlaceSubmit.isEnabled =
+                placeTitle != null
+                        && isTagSelected // 태그
+                        && ratingbarAddPlace.rating >= 1F // 별점
+                        && !etAddPlaceReview.text.isNullOrBlank() // 리뷰
+        }
+    }
+
+    private fun setOnSubmitButtonClicked() {
+        // TODO. 완료 버튼 클릭 시 API 연결
+
+        placeReviewContent = binding.etAddPlaceReview.text.toString()   // 리뷰 내용
+    }
+
+    private fun setPlaceTag() {
+        with(binding.chipGroup) {
+            setOnCheckedStateChangeListener { group, checkedIds ->
+                if (checkedIds.isEmpty()) {
+                    isTagSelected = false
+                } else {
+                    isTagSelected = true
+                    val selectedTag =
+                        findViewById<Chip>(binding.chipGroup.checkedChipId).text.toString()
+                    placeTag = selectedTag
+                }
+                activeSubmitButton()
+            }
+        }
+    }
+
+    private fun setPlaceRating() {
+        binding.ratingbarAddPlace.onRatingBarChangeListener = (
+                RatingBar.OnRatingBarChangeListener { ratingBar, rating, fromUser ->
+                    if (rating < 1.0F) {
+                        ratingBar?.rating = 1.0F
+                    } else {
+                        with(binding.tvAddPlaceRatingTitle) {
+                            text = when (rating) {
+                                1.0F, 1.5F -> "매우 별로"
+                                2.0F, 2.5F -> "별로"
+                                3.0F, 3.5F -> "보통"
+                                4.0F, 4.5F -> "좋아요"
+                                else -> "매우 좋아요"
+                            }
+                        }
+                    }
+                    placeStar = rating
+                    activeSubmitButton()
+                })
     }
 
     private fun setPlaceImageAdapter() {

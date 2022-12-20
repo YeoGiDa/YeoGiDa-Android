56aeb96fcb9e7ebd9e4d5c558c4e141be45edc66
hjh1161514 <hjh1161514@naver.com>
2022-12-20 20:23:37 +0900

    [FEAT] 나의 여행지 더미데이터 연결 및 기능 - 맨 위로 가기 - 뒤로 가기 - 여행지 추가 화면 연결 ui/#100

--- app/src/main/java/com/starters/yeogida/presentation/mypage/MyTripFragment.kt
+++ app/src/main/java/com/starters/yeogida/presentation/mypage/MyTripFragment.kt
@@ -27,6 +27,93 @@ class MyTripFragment : Fragment() {
         binding = FragmentMyTripBinding.inflate(inflater, container, false)
         binding.view = this
 
+        initClickListener()
+        initData()
+
         return binding.root
+    }
+
+    private fun initClickListener() {
+        binding.tbMyPlace.setNavigationOnClickListener {
+            findNavController().navigateUp()
+        }
+    }
+
+    private fun initData() {
+        val tripAdapter = LikeTripAdapter(
+            listOf(
+                LikeTripData(
+                    RegionCategory.SEOUL,
+                    "https://cdn.pixabay.com/photo/2022/10/08/06/31/hungarian-parliament-building-7506436_640.jpg",
+                    "여기는 어디",
+                    "여기는 헝가리",
+                    "호빵맨",
+                    true,
+                    3,
+                    9
+                ),
+                LikeTripData(
+                    RegionCategory.SEOUL,
+                    "https://cdn.pixabay.com/photo/2022/12/12/19/14/paris-7651738_1280.jpg",
+                    "여기는 어디",
+                    "여기는 프랑스",
+                    "세균맨",
+                    false,
+                    2,
+                    4
+                ),
+                LikeTripData(
+                    RegionCategory.SEOUL,
+                    "https://cdn.pixabay.com/photo/2022/12/01/00/13/antique-7627999_1280.jpg",
+                    "책벌레",
+                    "책을 읽어요",
+                    "식빵맨",
+                    true,
+                    0,
+                    2
+                ),
+                LikeTripData(
+                    RegionCategory.SEOUL,
+                    "https://cdn.pixabay.com/photo/2022/12/11/16/54/tree-7649287__480.jpg",
+                    "참새",
+                    "짹짹",
+                    "스프",
+                    true,
+                    13,
+                    11
+                ),
+                LikeTripData(
+                    RegionCategory.SEOUL,
+                    "https://cdn.pixabay.com/photo/2022/01/13/07/06/house-6934544__480.jpg",
+                    "한옥",
+                    "눈이 내려요",
+                    "고양이",
+                    true,
+                    2,
+                    1
+                ),
+                LikeTripData(
+                    RegionCategory.SEOUL,
+                    "https://cdn.pixabay.com/photo/2022/10/08/06/31/hungarian-parliament-building-7506436_640.jpg",
+                    "여기는 어디",
+                    "여기는 헝가리",
+                    "호빵맨",
+                    true,
+                    3,
+                    2
+                )
+            ),
+            viewModel
+        )
+        binding.rvMyTrip.adapter = tripAdapter
+    }
+
+    fun moveToTop(view: View) {
+        binding.scrollViewMyPlace.smoothScrollTo(0, 0)
+    }
+
+    fun moveToAddTrip(view: View) {
+        val intent = Intent(activity, AddTripActivity::class.java)
+        startActivity(intent)
     }
 }

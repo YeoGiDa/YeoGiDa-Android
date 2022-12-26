06d70715d3fa7859f0d5e06f7b1fe405a8135162
YOON <yys7517@naver.com>
2022-12-26 10:11:10 +0900

    [FEAT] 팔로워/팔로잉 유저 불러오기 API 연결 network/#130

--- app/src/main/java/com/starters/yeogida/data/remote/response/follow/FollowUserResponse.kt
+++ app/src/main/java/com/starters/yeogida/data/remote/response/follow/FollowUserResponse.kt
@@ -1,7 +1,5 @@
 package com.starters.yeogida.data.remote.response.follow
 
-import com.google.gson.annotations.SerializedName
-
 data class FollowerUserResponse(
     val followerList: List<FollowUserData>
 )
@@ -11,7 +9,7 @@ data class FollowingUserResponse(
 )
 
 data class FollowUserData(
-    @SerializedName("id") val memberId: Long,
-    @SerializedName("imgUrl") val userProfileImageUrl: String,
-    @SerializedName("nickname") val userNickname: String
+    val memberId: Long,
+    val nickname: String,
+    val imgUrl: String
 )
--- app/src/main/java/com/starters/yeogida/presentation/follow/FollowFragment.kt
+++ app/src/main/java/com/starters/yeogida/presentation/follow/FollowFragment.kt
@@ -1,6 +1,7 @@
 package com.starters.yeogida.presentation.follow
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,13 +13,20 @@ import com.starters.yeogida.R
 import com.starters.yeogida.YeogidaApplication
 import com.starters.yeogida.data.remote.response.follow.FollowUserData
 import com.starters.yeogida.databinding.FragmentFollowBinding
+import com.starters.yeogida.network.YeogidaClient
 import com.starters.yeogida.presentation.common.EventObserver
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class FollowFragment : Fragment() {
 
     private lateinit var binding: FragmentFollowBinding
     private val viewModel: FollowViewModel by viewModels()
     private val dataStore = YeogidaApplication.getInstance().getDataStore()
+    private val followService = YeogidaClient.followService
 
     companion object {
         val FOLLOW_CATEGORY_ITEM = "FOLLOW_CATEGORY_ITEM"
@@ -36,6 +44,13 @@ class FollowFragment : Fragment() {
         super.onCreate(savedInstanceState)
     }
 
+    override fun onResume() {
+        super.onResume()
+        Log.e("FollowFragment", "onResume()")
+        val choice = arguments?.getInt(FOLLOW_CATEGORY_ITEM)
+        setPage(choice)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -51,12 +66,70 @@ class FollowFragment : Fragment() {
 
         val choice = arguments?.getInt(FOLLOW_CATEGORY_ITEM)
         setPage(choice)
-        initFollowData()
+        initFollowerData()
         setOpenUserProfile()
     }
 
-    private fun initFollowData() {
-        TODO("Not yet implemented")
+    private fun initFollowerData() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val userBearerToken = dataStore.userBearerToken.first()
+
+            // 팔로워 목록
+            val followerUserResponse = followService.getFollowerUser(
+                userBearerToken
+            )
+
+            val followerList = followerUserResponse.body()?.data?.followerList
+
+            followerList?.let {
+                with(FollowLists.follower) {
+                    clear()
+                    for (i in followerList.indices) {
+                        add(
+                            FollowUserData(
+                                followerList[i].memberId,
+                                followerList[i].nickname,
+                                followerList[i].imgUrl
+                            )
+                        )
+                    }
+                }
+            }
+            withContext(Dispatchers.Main) {
+                binding.rvFollow.adapter?.notifyDataSetChanged()
+            }
+        }
+    }
+
+    private fun initFollowingData() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val userBearerToken = dataStore.userBearerToken.first()
+
+            // 팔로잉 목록
+            val followingUserResponse = followService.getFollowingUser(
+                userBearerToken
+            )
+
+            val followingList = followingUserResponse.body()?.data?.followingList
+
+            followingList?.let {
+                with(FollowLists.following) {
+                    clear()
+                    for (i in followingList.indices) {
+                        add(
+                            FollowUserData(
+                                followingList[i].memberId,
+                                followingList[i].nickname,
+                                followingList[i].imgUrl
+                            )
+                        )
+                    }
+                }
+            }
+            withContext(Dispatchers.Main) {
+                binding.rvFollow.adapter?.notifyDataSetChanged()
+            }
+        }
     }
 
     private fun setOpenUserProfile() {
@@ -71,8 +144,14 @@ class FollowFragment : Fragment() {
     private fun setPage(choice: Int?) {
         choice?.let {
             when (choice) {
-                0 -> setFollowAdapter(FollowLists.follower)
-                1 -> setFollowAdapter(FollowLists.following)
+                0 -> {
+                    setFollowAdapter(FollowLists.follower)
+                    initFollowerData()
+                }
+                1 -> {
+                    setFollowAdapter(FollowLists.following)
+                    initFollowingData()
+                }
                 else -> {}
             }
         }

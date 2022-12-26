51c7041828060552206ccb50398c7fd86663b090
YOON <yys7517@naver.com>
2022-12-26 11:54:03 +0900

    [FEAT] 팔로워/팔로잉 유저 삭제 API 연결 network/#130

--- app/src/main/java/com/starters/yeogida/data/api/FollowService.kt
+++ app/src/main/java/com/starters/yeogida/data/api/FollowService.kt
@@ -4,8 +4,10 @@ import com.starters.yeogida.data.remote.response.BaseResponse
 import com.starters.yeogida.data.remote.response.follow.FollowerUserResponse
 import com.starters.yeogida.data.remote.response.follow.FollowingUserResponse
 import retrofit2.Response
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Path
 
 interface FollowService {
     // 팔로워 목록 조회
@@ -19,4 +21,18 @@ interface FollowService {
     suspend fun getFollowingUser(
         @Header("Authorization") bearerToken: String
     ): Response<BaseResponse<FollowingUserResponse>>
+
+    // 팔로잉 삭제
+    @DELETE("follow/{toMemberId}/following")
+    suspend fun deleteFollowing(
+        @Header("Authorization") bearerToken: String,
+        @Path("toMemberId") memberId: Long
+    ): Response<BaseResponse<Boolean>>
+
+    // 팔로워 삭제
+    @DELETE("follow/{fromMemberId}/follower")
+    suspend fun deleteFollower(
+        @Header("Authorization") bearerToken: String,
+        @Path("fromMemberId") memberId: Long
+    ): Response<BaseResponse<Boolean>>
 }
--- app/src/main/java/com/starters/yeogida/presentation/follow/FollowFragment.kt
+++ app/src/main/java/com/starters/yeogida/presentation/follow/FollowFragment.kt
@@ -14,7 +14,9 @@ import com.starters.yeogida.YeogidaApplication
 import com.starters.yeogida.data.remote.response.follow.FollowUserData
 import com.starters.yeogida.databinding.FragmentFollowBinding
 import com.starters.yeogida.network.YeogidaClient
+import com.starters.yeogida.presentation.common.CustomDialog
 import com.starters.yeogida.presentation.common.EventObserver
+import com.starters.yeogida.util.shortToast
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.first
@@ -70,6 +72,7 @@ class FollowFragment : Fragment() {
         setOpenUserProfile()
     }
 
+
     private fun initFollowerData() {
         CoroutineScope(Dispatchers.IO).launch {
             val userBearerToken = dataStore.userBearerToken.first()
@@ -78,25 +81,34 @@ class FollowFragment : Fragment() {
             val followerUserResponse = followService.getFollowerUser(
                 userBearerToken
             )
+            when (followerUserResponse.code()) {
+                200 -> {
+                    val followerList = followerUserResponse.body()?.data?.followerList
 
-            val followerList = followerUserResponse.body()?.data?.followerList
+                    followerList?.let {
+                        with(FollowLists.follower) {
+                            clear()
+                            for (i in followerList.indices) {
+                                add(
+                                    FollowUserData(
+                                        followerList[i].memberId,
+                                        followerList[i].nickname,
+                                        followerList[i].imgUrl
+                                    )
+                                )
+                            }
+                        }
+                    }
 
-            followerList?.let {
-                with(FollowLists.follower) {
-                    clear()
-                    for (i in followerList.indices) {
-                        add(
-                            FollowUserData(
-                                followerList[i].memberId,
-                                followerList[i].nickname,
-                                followerList[i].imgUrl
-                            )
-                        )
+                    withContext(Dispatchers.Main) {
+                        binding.rvFollow.adapter?.notifyDataSetChanged()
                     }
                 }
-            }
-            withContext(Dispatchers.Main) {
-                binding.rvFollow.adapter?.notifyDataSetChanged()
+
+                else -> {
+                    Log.e("FollowerResponse", "팔로워 불러오기 실패 $followerUserResponse")
+                    requireContext().shortToast("팔로워 불러오기 실패")
+                }
             }
         }
     }
@@ -110,24 +122,32 @@ class FollowFragment : Fragment() {
                 userBearerToken
             )
 
-            val followingList = followingUserResponse.body()?.data?.followingList
+            when (followingUserResponse.code()) {
+                200 -> {
+                    val followingList = followingUserResponse.body()?.data?.followingList
 
-            followingList?.let {
-                with(FollowLists.following) {
-                    clear()
-                    for (i in followingList.indices) {
-                        add(
-                            FollowUserData(
-                                followingList[i].memberId,
-                                followingList[i].nickname,
-                                followingList[i].imgUrl
-                            )
-                        )
+                    followingList?.let {
+                        with(FollowLists.following) {
+                            clear()
+                            for (i in followingList.indices) {
+                                add(
+                                    FollowUserData(
+                                        followingList[i].memberId,
+                                        followingList[i].nickname,
+                                        followingList[i].imgUrl
+                                    )
+                                )
+                            }
+                        }
+                    }
+                    withContext(Dispatchers.Main) {
+                        binding.rvFollow.adapter?.notifyDataSetChanged()
                     }
                 }
-            }
-            withContext(Dispatchers.Main) {
-                binding.rvFollow.adapter?.notifyDataSetChanged()
+                else -> {
+                    Log.e("FollowerResponse", "팔로잉 목록 불러오기 실패 $followingUserResponse")
+                    requireContext().shortToast("팔로잉 목록 불러오기 실패")
+                }
             }
         }
     }
@@ -145,11 +165,11 @@ class FollowFragment : Fragment() {
         choice?.let {
             when (choice) {
                 0 -> {
-                    setFollowAdapter(FollowLists.follower)
+                    setFollowAdapter(FollowLists.follower, choice)
                     initFollowerData()
                 }
                 1 -> {
-                    setFollowAdapter(FollowLists.following)
+                    setFollowAdapter(FollowLists.following, choice)
                     initFollowingData()
                 }
                 else -> {}
@@ -157,7 +177,78 @@ class FollowFragment : Fragment() {
         }
     }
 
-    private fun setFollowAdapter(followUserResponseList: List<FollowUserData>) {
-        binding.rvFollow.adapter = FollowAdapter(followUserResponseList, viewModel)
+    private fun setFollowAdapter(followUserResponseList: List<FollowUserData>, choice: Int) {
+
+        with(binding.rvFollow) {
+            adapter = FollowAdapter(followUserResponseList, viewModel).apply {
+                itemClick = object : FollowAdapter.ItemClick {
+                    override fun onDeleteBtnClick(view: View, user: FollowUserData) {
+                        showDialog(choice, user)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun showDialog(choice: Int, user: FollowUserData) {
+        CustomDialog(requireContext()).apply {
+            setTitle("${if (choice == 0) "팔로워" else "팔로잉"} 목록에서 삭제합니다.")
+            setPositiveBtn("삭제") {
+                deleteFollowUser(choice, user)
+                dismissDialog()
+            }
+            setNegativeBtn("취소") {
+                dismissDialog()
+            }
+            showDialog()
+        }
+    }
+
+    private fun deleteFollowUser(choice: Int, user: FollowUserData) {
+        if (choice == 0) {
+            // 팔로워 삭제
+            CoroutineScope(Dispatchers.IO).launch {
+                val userBearerToken = dataStore.userBearerToken.first()
+
+                val response = followService.deleteFollower(
+                    userBearerToken,
+                    user.memberId
+                )
+
+                when (response.code()) {
+                    200 -> {
+                        withContext(Dispatchers.Main) {
+                            FollowLists.follower.remove(user)   // 목록에서 삭제
+                            binding.rvFollow.adapter?.notifyDataSetChanged()
+                        }
+                    }
+                    else -> {
+                        Log.e("deleteFollow", response.toString())
+                    }
+                }
+            }
+        } else {
+            // 팔로잉 삭제
+            CoroutineScope(Dispatchers.IO).launch {
+                val userBearerToken = dataStore.userBearerToken.first()
+
+                val response = followService.deleteFollowing(
+                    userBearerToken,
+                    user.memberId
+                )
+
+                when (response.code()) {
+                    200 -> {
+                        withContext(Dispatchers.Main) {
+                            FollowLists.following.remove(user)   // 목록에서 삭제
+                            binding.rvFollow.adapter?.notifyDataSetChanged()
+                        }
+                    }
+                    else -> {
+                        Log.e("deleteFollow", response.toString())
+                    }
+                }
+            }
+        }
     }
 }

9357acd3daca791de0916ac7bbcda34fcdda3d92
hjh1161514 <hjh1161514@naver.com>
2022-12-27 20:35:13 +0900

    [FEAT] 둘러보기 마커 API response network/#134

---
+++ app/src/main/java/com/starters/yeogida/data/remote/response/place/ClusterMarkerResponse.kt
@@ -0,0 +1,12 @@
+package com.starters.yeogida.data.remote.response.place
+
+data class ClusterMarkerResponse(
+    val placeList: List<ClusterMarkerData>
+)
+
+data class ClusterMarkerData(
+    val placeId: Long,
+    val imgUrl: String,
+    val nickname: String,
+    val star: Float
+)

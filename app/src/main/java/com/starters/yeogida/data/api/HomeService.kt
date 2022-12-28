bb2fecbcad99dd77f0d70174532a9949c7b09942
hjh1161514 <hjh1161514@naver.com>
2022-12-28 17:23:35 +0900

    [FEAT] 팔로잉의 최근 게시글 API 추가 network/#162

--- app/src/main/java/com/starters/yeogida/data/api/HomeService.kt
+++ app/src/main/java/com/starters/yeogida/data/api/HomeService.kt
@@ -2,14 +2,23 @@ package com.starters.yeogida.data.api
 
 import com.starters.yeogida.data.remote.response.BaseResponse
 import com.starters.yeogida.data.remote.response.BestTravelerResponse
-import com.starters.yeogida.data.remote.response.MonthlyBestResponse
+import com.starters.yeogida.data.remote.response.HomeTripResponseResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Header
 
 interface HomeService {
+    // 월간 베스트 여행지
     @GET("trips/monthly-best/basic")
-    fun getMonthlyBest(): Call<BaseResponse<MonthlyBestResponse>>
+    fun getMonthlyBest(): Call<BaseResponse<HomeTripResponseResponse>>
 
+    // best traveler
     @GET("members/best-traveler/basic")
     fun getBestTraveler(): Call<BaseResponse<BestTravelerResponse>>
+
+    // 팔로잉의 최근 게시글
+    @GET("trips/follow/basic")
+    fun getRecentTrip(
+        @Header("Authorization") token: String
+    ): Call<BaseResponse<HomeTripResponseResponse>>
 }

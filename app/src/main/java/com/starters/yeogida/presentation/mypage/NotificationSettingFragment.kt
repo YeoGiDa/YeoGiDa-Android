b412d2523aaaa734f9182b6acc9efab4be7dc6ed
hjh1161514 <hjh1161514@naver.com>
2022-12-28 11:44:37 +0900

    [FEAT] 알림 상태 datastore에 저장 및 불러오기 feature/#149

--- app/src/main/java/com/starters/yeogida/presentation/mypage/NotificationSettingFragment.kt
+++ app/src/main/java/com/starters/yeogida/presentation/mypage/NotificationSettingFragment.kt
@@ -6,10 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.starters.yeogida.YeogidaApplication
 import com.starters.yeogida.databinding.FragmentNotificationSettingBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 class NotificationSettingFragment : Fragment() {
     private lateinit var binding: FragmentNotificationSettingBinding
+    private val dataStore = YeogidaApplication.getInstance().getDataStore()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,10 +29,25 @@ class NotificationSettingFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initSwitchChecked()
         initClickListener()
     }
 
-    // 각 알림 switch check 변경 시
+    private fun initSwitchChecked() {
+        CoroutineScope(Dispatchers.IO).launch {
+            with(binding) {
+                switchNotificationFollow.isChecked = dataStore.notificationFollowIsAllow.first()
+                switchNotificationLike.isChecked = dataStore.notificationLikeIsAllow.first()
+                switchNotificationComment.isChecked = dataStore.notificationCommentIsAllow.first()
+
+                if (switchNotificationComment.isChecked && switchNotificationLike.isChecked && switchNotificationFollow.isChecked) {
+                    switchNotificationAll.isChecked = true
+                }
+            }
+        }
+    }
+
+// 각 알림 switch check 변경 시
     fun changeSwitch(view: View) {
         with(binding) {
             // 각 알림 switch check를 모두 true 변경 시
@@ -34,10 +55,12 @@ class NotificationSettingFragment : Fragment() {
                 switchNotificationAll.isChecked = true
             }
 
-            // 각 알림 switch check를 모두 false로 변경 시
+            // 각 알림 switch check 중 false로 변경 시
             if (!switchNotificationComment.isChecked || !switchNotificationLike.isChecked || !switchNotificationFollow.isChecked) {
                 switchNotificationAll.isChecked = false
             }
+
+            saveNotification(like = switchNotificationLike.isChecked, follow = switchNotificationFollow.isChecked, comment = switchNotificationComment.isChecked)
         }
     }
 
@@ -45,8 +68,10 @@ class NotificationSettingFragment : Fragment() {
     fun changeSwitchAll(view: View) {
         if (binding.switchNotificationAll.isChecked) {
             isCheckedNotification(b1 = true, b2 = true, b3 = true)
+            saveNotification(like = true, follow = true, comment = true)
         } else {
             isCheckedNotification(b1 = false, b2 = false, b3 = false)
+            saveNotification(like = false, follow = false, comment = false)
         }
     }
 
@@ -55,6 +80,14 @@ class NotificationSettingFragment : Fragment() {
             switchNotificationComment.isChecked = b1
             switchNotificationLike.isChecked = b2
             switchNotificationFollow.isChecked = b3
+        }
+    }
+
+    private fun saveNotification(like: Boolean, follow: Boolean, comment: Boolean) {
+        CoroutineScope(Dispatchers.IO).launch {
+            dataStore.saveNotificationLikeIsAllow(like)
+            dataStore.saveNotificationFollowIsAllow(follow)
+            dataStore.saveNotificationCommentIsAllow(comment)
         }
     }
 

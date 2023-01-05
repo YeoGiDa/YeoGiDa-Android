af82dde13580bdb4a1ebd732562d2d950e5873d3
hjh1161514 <hjh1161514@naver.com>
2023-01-05 12:05:35 +0900

    [FEAT] Context 추가 feature/#214

--- app/src/main/java/com/starters/yeogida/YeogidaApplication.kt
+++ app/src/main/java/com/starters/yeogida/YeogidaApplication.kt
@@ -1,15 +1,20 @@
 package com.starters.yeogida
 
 import android.app.Application
+import android.content.Context
 import com.kakao.sdk.common.KakaoSdk
 
 class YeogidaApplication : Application() {
-
     private lateinit var dataStore: YeogidaDataStore
 
     companion object {
-         private lateinit var yeogidaApplication: YeogidaApplication
-         fun getInstance() : YeogidaApplication = yeogidaApplication
+        private lateinit var yeogidaApplication: YeogidaApplication
+        // var instance: YeogidaApplication? = null
+
+        fun getInstance(): YeogidaApplication = yeogidaApplication
+        fun context(): Context {
+            return getInstance().applicationContext
+        }
     }
 
     override fun onCreate() {

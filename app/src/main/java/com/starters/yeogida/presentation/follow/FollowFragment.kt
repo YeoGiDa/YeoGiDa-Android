f5b2734c337f304c6021ac6744e7d17291c92e6d
YOON <yys7517@naver.com>
2023-01-05 16:38:39 +0900

    [REFACTOR] 코드 리뷰 반영 ui/#220

--- app/src/main/java/com/starters/yeogida/presentation/follow/FollowFragment.kt
+++ app/src/main/java/com/starters/yeogida/presentation/follow/FollowFragment.kt
@@ -232,40 +232,38 @@ class FollowFragment : Fragment() {
                 0 -> {
                     setFollowAdapter(FollowLists.follower, choice)
                     initFollowerData()
-                    isEmptyView(FollowLists.follower)
-                    if (isEmptyView(FollowLists.follower)) {
-                        binding.tvFollowEmpty.text = "아직 회원님을 팔로우한 사람이 없어요!"
-                    } else {
-                    }
+                    setEmptyView(
+                        FollowLists.follower,
+                        "아직 회원님을 팔로우한 사람이 없어요!"
+                    )
                 }
                 1 -> {
                     setFollowAdapter(FollowLists.following, choice)
                     initFollowingData()
-                    if (isEmptyView(FollowLists.following)) {
-                        binding.tvFollowEmpty.text = "아직 팔로잉한 사람이 없어요\n" +
-                                "사람들을 팔로잉 해보세요!"
-                    } else {
-                    }
+                    setEmptyView(
+                        FollowLists.following,
+                        "아직 팔로잉한 사람이 없어요\n" + "사람들을 팔로잉 해보세요!"
+                    )
                 }
                 else -> {}
             }
         }
     }
 
-    private fun isEmptyView(list: List<FollowUserData>): Boolean {
+    private fun setEmptyView(list: List<FollowUserData>, message: String) {
         with(binding) {
             if (list.isEmpty()) {
                 etSearch.visibility = View.GONE
                 ivFollowSearch.visibility = View.GONE
                 rvFollow.visibility = View.GONE
                 layoutFollowEmpty.visibility = View.VISIBLE
-                return true
+                binding.tvFollowEmpty.text = message
             } else {
                 etSearch.visibility = View.VISIBLE
                 ivFollowSearch.visibility = View.VISIBLE
                 rvFollow.visibility = View.VISIBLE
                 layoutFollowEmpty.visibility = View.GONE
-                return false
+                binding.tvFollowEmpty.text = message
             }
         }
     }

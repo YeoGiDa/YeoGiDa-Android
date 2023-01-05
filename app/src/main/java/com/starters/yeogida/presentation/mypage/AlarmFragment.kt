4743c9b11e48b34b9e2cc4485c1fa058ce6457b6
hjh1161514 <hjh1161514@naver.com>
2023-01-05 18:21:06 +0900

    [FEAT] 알림 목록 엠티뷰 ui/#226

--- app/src/main/java/com/starters/yeogida/presentation/mypage/AlarmFragment.kt
+++ app/src/main/java/com/starters/yeogida/presentation/mypage/AlarmFragment.kt
@@ -2,7 +2,6 @@ package com.starters.yeogida.presentation.mypage
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,16 +9,11 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.starters.yeogida.R
-import com.starters.yeogida.YeogidaApplication
 import com.starters.yeogida.databinding.FragmentAlarmBinding
 import com.starters.yeogida.network.YeogidaClient
 import com.starters.yeogida.presentation.place.PlaceActivity
 import com.starters.yeogida.presentation.user.profile.UserProfileActivity
 import com.starters.yeogida.util.customEnqueue
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.launch
 
 class AlarmFragment : Fragment() {
     private lateinit var binding: FragmentAlarmBinding
@@ -40,20 +34,25 @@ class AlarmFragment : Fragment() {
     }
 
     private fun initAdapter() {
-        val notificationAdapter = NotificationAdapter { type: String, followerId: Long, placeId: Long, tripId: Long  ->
-            moveToPage(type, followerId, placeId, tripId)
-        }
+        val notificationAdapter =
+            NotificationAdapter { type: String, followerId: Long, placeId: Long, tripId: Long ->
+                moveToPage(type, followerId, placeId, tripId)
+            }
         binding.rvNotification.adapter = notificationAdapter
 
-            YeogidaClient.myPageService.getNotificationList().customEnqueue(
-                onSuccess = {
-                    if (it.code == 200) {
+        YeogidaClient.myPageService.getNotificationList().customEnqueue(
+            onSuccess = {
+                if (it.code == 200) {
+                    if (it.data?.alarmList?.isEmpty() == true) {
+                        binding.layoutAlarmEmpty.visibility = View.VISIBLE
+                    } else {
+                        binding.layoutAlarmEmpty.visibility = View.GONE
                         it.data?.let { data -> notificationAdapter.notificationList.addAll(data.alarmList) }
                         notificationAdapter.notifyDataSetChanged()
                     }
                 }
-            )
-
+            }
+        )
     }
 
     private fun initNavigation() {

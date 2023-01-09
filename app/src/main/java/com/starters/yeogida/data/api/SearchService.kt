f31685ad0b6aa9b9be79918245e451f85bc0d8ef
YOON <yys7517@naver.com>
2023-01-09 16:53:09 +0900

    [FEAT] 검색 결과 서비스 함수 작성, Response DTO 작성 network/#249

--- app/src/main/java/com/starters/yeogida/data/api/SearchService.kt
+++ app/src/main/java/com/starters/yeogida/data/api/SearchService.kt
@@ -2,8 +2,10 @@ package com.starters.yeogida.data.api
 
 import com.starters.yeogida.data.remote.response.BaseResponse
 import com.starters.yeogida.data.remote.response.trip.PopularTripResponse
+import com.starters.yeogida.data.remote.response.trip.TripSearchResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface SearchService {
 
@@ -11,4 +13,10 @@ interface SearchService {
     @GET("trips/search/ranking")
     suspend fun getPopularTrip(): Response<BaseResponse<PopularTripResponse>>
 
+    // 검색 결과 가져오기 (정렬, 검색어)
+    @GET("trips/search")
+    suspend fun getSearchResult(
+        @Query("keyword") searchKeyword: String,
+        @Query("condition") condition: String
+    ): Response<BaseResponse<TripSearchResponse>>
 }
